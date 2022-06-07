355073fbc635b2f9f7c6aa7ad724ad7d4b669476
Saikat Ray <Saikat.Ray@hyland.com>
2022-06-07 23:13:11 +0530

    functions

--- Cards/main.go
+++ Cards/main.go
@@ -7,8 +7,12 @@ var deckSize int
 func main() {
 	//var card string = "Ace of Spades"
 	card := "Ace of Spades" //only used for new variable not already declared variables
-	card = "Five of Diamonds"
+	card = newCard()
 	fmt.Println(card)
 	deckSize = 25
 	fmt.Println(deckSize)
+}
+
+func newCard() string {
+	return "Five Of Spades"
 }
