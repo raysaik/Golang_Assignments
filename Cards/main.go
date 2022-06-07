95857e6c5275dff4c6895dfb82cff73814c853f0
Saikat Ray <Saikat.Ray@hyland.com>
2022-06-07 22:57:48 +0530

    cards added

---
+++ Cards/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var deckSize int
+
+func main() {
+	//var card string = "Ace of Spades"
+	card := "Ace of Spades" //only used for new variable not already declared variables
+	card = "Five of Diamonds"
+	fmt.Println(card)
+	deckSize = 25
+	fmt.Println(deckSize)
+}
