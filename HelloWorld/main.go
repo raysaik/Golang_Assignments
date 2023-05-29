3ba0330d6aa5351053d83169409c5c29ab5a17d5
Saikat Ray <sray@cinemo.com>
2023-05-29 09:11:19 +0200

    golang_exercise

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"math"
+	"reflect"
 )
 
 func main() {
 	fmt.Println("Hi There!")
+	fmt.Println(math.Floor(2.141))
+	fmt.Println(reflect.TypeOf(25))
+	fmt.Println(reflect.TypeOf(2.5))
+	fmt.Println(reflect.TypeOf(2.5))
+	fmt.Println(reflect.TypeOf(true))
+	fmt.Println(reflect.TypeOf("Hello"))
+	fmt.Println(reflect.TypeOf('A'))
+
 	fmt.Printf("The sqrt number is %g for now. ", math.Sqrt(7))
 }
